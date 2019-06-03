35f516b13150057bc407c877d67b9eaacc61ba03
Celia <celiafrom12@gmail.com>
2019-06-03 14:55:46 +0100

    top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,9 @@ describe Oystercard do
   it 'shows balance' do
     expect(subject.balance).to eq(0)
   end
+
+  it '#top up' do
+    card = Oystercard.new
+    expect{ card.top_up 1 }.to change{ card.balance }.by 1
+  end
 end
