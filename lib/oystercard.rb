dca9f9a959733b220e60d5cdd62a75070bae4c16
Student <student@makerss-mbp-6.home>
2019-06-05 22:14:38 +0100

    Journey history added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,15 @@
 class Oystercard
-  attr_reader :balance, :in_use, :BAL_LIMIT, :MIN_BAL
+  attr_reader :balance, :in_use, :BAL_LIMIT, :MIN_BAL, :entry_station, :exit_station
   BAL_LIMIT = 90
   MIN_BAL = 1
+  MIN_CHARGE = 5
 
   def initialize
     @balance = 0
     @in_use = false
+    @entry_station = nil
+    @exit_station = nil
+    @journeys = []
   end
 
   def top_up(amount)
@@ -13,22 +17,39 @@ class Oystercard
     @balance += amount
   end
 
+  def in_journey?
+    @entry_station != nil ? @in_journey = true : @in_journey = false
+  end
+
+  def touch_in(station)
+    fail "Oystercard balance insufficient" if @balance < MIN_BAL
+    @in_use = true
+    @entry_station = station
+  end
+
+  def touch_out(station)
+    deduct(MIN_CHARGE)
+    @in_use = false
+    @exit_station = station
+  end
+
+  def entry_station
+    @entry_station
+  end
+
+  def exit_station
+    @exit_station
+  end
+
+  def journey_history
+    journey = {@entry_station => @exit_station}
+    @journeys << journey
+  end
+
+private
+
   def deduct(amount)
     @balance -= amount
   end
-
-  def in_journey?
-    @in_use
-  end
-
-  def touch_in
-    fail "Oystercard balance insufficient" if @balance < MIN_BAL
-    @in_use = true
-  end
-
-  def touch_out
-    @in_use = false
-  end
-
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
   subject { Oystercard.new }
-
+  let(:station){ double :station }
+  let(:entry_station) { double :station }
+  let(:exit_station) { double :station }
+  let(:journeys) { {entry_station: entry_station, exit_station: exit_station} }
   it 'shows balance' do
     expect(subject.balance).to eq(0)
   end
@@ -17,14 +20,14 @@ describe Oystercard do
    expect { subject.top_up 1 }.to raise_error("Maximum balance of #{maximum}")
   end
 
-  describe '#deduct' do
-    it 'takes an argument and deducts it from the balance' do
-      subject.top_up(45)
-#     subject.deduct(5)
-#     expect(subject.balance).to eq(40)
-      expect(subject.deduct(5)).to eq(40)
-    end
-  end
+#   describe '#deduct' do
+#     it 'takes an argument and deducts it from the balance' do
+#       subject.top_up(45)
+# #     subject.deduct(5)
+# #     expect(subject.balance).to eq(40)
+#       expect(subject.deduct(5)).to eq(40)
+  #   end
+  # end
 
     it 'responds to if the customer is in journey' do
       expect(subject.in_journey?).to be_falsey
@@ -32,15 +35,46 @@ describe Oystercard do
 
     it 'allows customer to touch in' do
       subject.top_up(1)
-      subject.touch_in
+      subject.touch_in(station)
       expect(subject.in_journey?).to eq true
     end
 
     it 'allows customer to touch out' do
-      subject.touch_out
+      subject.touch_out(station)
       expect(subject.in_journey?).to eq false
     end
+
     it 'raises error when balance in insufficient' do
-      expect { subject.touch_in }.to raise_error("Oystercard balance insufficient")
+      expect { subject.touch_in(station) }.to raise_error("Oystercard balance insufficient")
+    end
+
+    it 'charges the customer at touch out' do
+      subject.top_up(5)
+      subject.touch_in(station)
+      expect{ subject.touch_out(station) }.to change{ subject.balance }.by(-Oystercard::MIN_CHARGE)
+    end
+
+    it 'has an empty list of journeys by default' do
+      expect(subject.journey_history).to be_empty
+    end
+    
+    it 'stores the entry station' do
+      subject.top_up(5)
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq station
+    end
+
+    it 'stores exit station' do
+      subject.top_up(5)
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.exit_station).to eq exit_station
+    end
+
+    it 'stores one complete journey' do
+      subject.top_up(5)
+      subject.touch_in("Ilford")
+      subject.touch_out("Mile End")
+      expect(subject.journey_history).to eq ["Ilford" => "Mile End"]
     end
 end
